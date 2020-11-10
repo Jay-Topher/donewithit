ff3ba8fd61883c9b89402e58e17ebdc45d7ae794
Jones Ogolo <jay.topher11@gmail.com>
2020-11-10 20:53:32 +0100

    mounted account screen on app

--- App.js
+++ App.js
@@ -4,13 +4,16 @@ import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 import AppButton from "./app/components/AppButton";
 import AppText from "./app/components/AppText";
 import Card from "./app/components/Card";
+import Icon from "./app/components/Icon";
+import Screen from "./app/components/Screen";
+import AccountScreen from "./app/screens/AccountScreen";
 import ImageView from "./app/screens/ImageView";
 import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 import MessagesScreen from "./app/screens/MessagesScreen";
 import StartScreen from "./app/screens/StartScreen";
 
 export default function App() {
-  return <MessagesScreen />;
+  return <AccountScreen />;
 }
 
 const styles = StyleSheet.create({
