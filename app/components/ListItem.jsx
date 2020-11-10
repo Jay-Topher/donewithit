b9f5a2d6a35e69b0ef6d30423decea5e5e80d533
Jones Ogolo <jay.topher11@gmail.com>
2020-11-10 20:52:41 +0100

    added swipeable to list item

--- app/components/ListItem.jsx
+++ app/components/ListItem.jsx
@@ -6,20 +6,32 @@ import {
   TouchableHighlight,
   View,
 } from "react-native";
+import Swipeable from "react-native-gesture-handler/Swipeable";
+
 import colors from "../config/colors";
 import AppText from "./AppText";
 
-const ListItem = ({ image, title, subTitle, onPress }) => {
+const ListItem = ({
+  image,
+  title,
+  subTitle,
+  onPress,
+  renderRightActions,
+  IconComponent,
+}) => {
   return (
-    <TouchableHighlight onPress={onPress} underlayColor={colors.light}>
-      <View style={styles.container}>
-        <Image source={image} style={styles.image} />
-        <View>
-          <AppText style={styles.title}>{title}</AppText>
-          <AppText style={styles.subTitle}>{subTitle}</AppText>
+    <Swipeable renderRightActions={renderRightActions}>
+      <TouchableHighlight onPress={onPress} underlayColor={colors.light}>
+        <View style={styles.container}>
+          {IconComponent}
+          {image && <Image source={image} style={styles.image} />}
+          <View style={styles.detailsContainer}>
+            <AppText style={styles.title}>{title}</AppText>
+            {subTitle && <AppText style={styles.subTitle}>{subTitle}</AppText>}
+          </View>
         </View>
-      </View>
-    </TouchableHighlight>
+      </TouchableHighlight>
+    </Swipeable>
   );
 };
 
@@ -29,12 +41,16 @@ const styles = StyleSheet.create({
   container: {
     flexDirection: "row",
     padding: 15,
+    backgroundColor: colors.white,
+  },
+  detailsContainer: {
+    marginLeft: 10,
+    justifyContent: "center",
   },
   image: {
     width: 70,
     height: 70,
     borderRadius: 35,
-    marginRight: 10,
   },
   subTitle: {
     color: colors.medium,
