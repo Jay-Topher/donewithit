b617e9b382ca1e41bf25eef43ff0d52ece5e2296
Jones Ogolo <jay.topher11@gmail.com>
2020-11-11 23:34:57 +0100

    created app text input screen

---
+++ app/components/AppTextInput.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { StyleSheet, View, TextInput } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import colors from "../config/colors";
+import defaultStyles from "../config/styles";
+
+const AppTextInput = ({ icon, ...props }) => {
+  return (
+    <View style={styles.inputContainer}>
+      {icon && (
+        <MaterialCommunityIcons
+          name={icon}
+          size={20}
+          color={colors.medium}
+          style={styles.icon}
+        />
+      )}
+      <TextInput style={styles.input} {...props} />
+    </View>
+  );
+};
+
+export default AppTextInput;
+
+const styles = StyleSheet.create({
+  icon: {
+    marginRight: 10,
+  },
+  input: {
+    flex: 1,
+    ...defaultStyles.text,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    backgroundColor: colors.light,
+    height: 40,
+    alignItems: "center",
+    paddingHorizontal: 15,
+    borderRadius: 20,
+    width: "100%",
+    marginVertical: 10,
+  },
+});
