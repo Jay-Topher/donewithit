f575e856cc2ddeeada6aad670f6f3ae54e754c0f
Jones Ogolo <jay.topher11@gmail.com>
2020-11-13 23:16:09 +0100

    exported all form components

---
+++ app/components/forms/index.jsx
@@ -0,0 +1,21 @@
+// export { default as AppForm } from "./AppForm";
+// export { default as AppFormField } from "./AppFormField";
+// export { default as AppTextInput } from "./AppTextInput";
+// export { default as ErrorMessage } from "./ErrorMessage";
+// export { default as SubmitButton } from "./SubmitButton";
+
+import AppForm from "./AppForm";
+import AppFormField from "./AppFormField";
+import AppFormPicker from "./AppFormPicker";
+import AppTextInput from "./AppTextInput";
+import ErrorMessage from "./ErrorMessage";
+import SubmitButton from "./SubmitButton";
+
+export {
+  AppForm,
+  AppFormField,
+  AppFormPicker,
+  AppTextInput,
+  ErrorMessage,
+  SubmitButton,
+};
