05bd6d9c2550d0a6fedb08835f3995b27a555af0
Jones Ogolo <jay.topher11@gmail.com>
2020-11-18 13:56:35 +0100

    integrate with formik

---
+++ app/components/forms/FormImagePicker.jsx
@@ -0,0 +1,31 @@
+import { useFormikContext } from "formik";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import ImageInputList from "../ImageInputList";
+import ErrorMessage from "./ErrorMessage";
+
+const FormImagePicker = ({ name }) => {
+  const { errors, setFieldValue, touched, values } = useFormikContext();
+  const imageUris = values[name];
+  const handleAdd = (uri) => setFieldValue(name, [...imageUris, uri]);
+  const handleRemove = (uri) => {
+    setFieldValue(
+      name,
+      imageUris.filter((imageUri) => imageUri !== uri)
+    );
+  };
+  return (
+    <>
+      <ImageInputList
+        imageUri={imageUris}
+        onAddImage={handleAdd}
+        onRemoveImage={handleRemove}
+      />
+      <ErrorMessage error={errors[name]} visible={touched[name]} />
+    </>
+  );
+};
+
+export default FormImagePicker;
+
+const styles = StyleSheet.create({});
