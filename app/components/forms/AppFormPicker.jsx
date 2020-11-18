8707ba4d888ee51877b03bd0d42655de53ac2e9b
Jones Ogolo <jay.topher11@gmail.com>
2020-11-18 13:55:57 +0100

    more props

--- app/components/forms/AppFormPicker.jsx
+++ app/components/forms/AppFormPicker.jsx
@@ -4,15 +4,25 @@ import { StyleSheet, Text, View } from "react-native";
 import { ErrorMessage } from ".";
 import AppPicker from "../AppPicker";
 
-const AppFormPicker = ({ items, name, placeholder }) => {
+const AppFormPicker = ({
+  items,
+  name,
+  numberOfColumns,
+  placeholder,
+  PickerItemComponent,
+  width,
+}) => {
   const { errors, setFieldValue, touched, values } = useFormikContext();
   return (
     <>
       <AppPicker
         items={items}
+        numberOfColumns={numberOfColumns}
         onSelectItem={(item) => setFieldValue(name, item)}
+        PickerItemComponent={PickerItemComponent}
         placeholder={placeholder}
         selectedItem={values[name]}
+        width={width}
       />
       <ErrorMessage error={errors[name]} visible={touched[name]} />
     </>
