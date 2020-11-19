94a61fef38ace5c1670dbd0d3565fef46b8c5de6
Jones Ogolo <jay.topher11@gmail.com>
2020-11-19 21:54:47 +0100

    revert to old import

--- app/components/forms/index.jsx
+++ app/components/forms/index.jsx
@@ -1,21 +1,22 @@
-// export { default as AppForm } from "./AppForm";
-// export { default as AppFormField } from "./AppFormField";
-// export { default as AppTextInput } from "./AppTextInput";
-// export { default as ErrorMessage } from "./ErrorMessage";
-// export { default as SubmitButton } from "./SubmitButton";
+export { default as AppForm } from "./AppForm";
+export { default as AppFormField } from "./AppFormField";
+export { default as AppFormPicker } from "./AppFormPicker";
+export { default as AppTextInput } from "./AppTextInput";
+export { default as ErrorMessage } from "./ErrorMessage";
+export { default as SubmitButton } from "./SubmitButton";
 
-import AppForm from "./AppForm";
-import AppFormField from "./AppFormField";
-import AppFormPicker from "./AppFormPicker";
-import AppTextInput from "./AppTextInput";
-import ErrorMessage from "./ErrorMessage";
-import SubmitButton from "./SubmitButton";
+// import AppForm from "./AppForm";
+// import AppFormField from "./AppFormField";
+// import AppFormPicker from "./AppFormPicker";
+// import AppTextInput from "./AppTextInput";
+// import ErrorMessage from "./ErrorMessage";
+// import SubmitButton from "./SubmitButton";
 
-export {
-  AppForm,
-  AppFormField,
-  AppFormPicker,
-  AppTextInput,
-  ErrorMessage,
-  SubmitButton,
-};
+// export {
+//   AppForm,
+//   AppFormField,
+//   AppFormPicker,
+//   AppTextInput,
+//   ErrorMessage,
+//   SubmitButton,
+// };
