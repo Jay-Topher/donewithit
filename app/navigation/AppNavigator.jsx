2a44e6dffa6edb82b7c7bf42fde1ad534e736ce6
Jones Ogolo <jay.topher11@gmail.com>
2020-11-25 09:35:45 +0100

    created navigation

---
+++ app/navigation/AppNavigator.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+import ListingEditScreen from "../screens/ListingEditScreen";
+import FeedStackNavigator from "./FeedNavigator";
+import AccountStackNavigator from "./AccountNavigator";
+import NewListingButton from "./NewListingButton";
+import routes from "./routes";
+
+const Tab = createBottomTabNavigator();
+
+const AppTabNavigator = () => (
+  <Tab.Navigator>
+    <Tab.Screen
+      name={routes.FEED}
+      component={FeedStackNavigator}
+      options={{
+        tabBarIcon: ({ color, size }) => (
+          <MaterialCommunityIcons name="home" color={color} size={size} />
+        ),
+      }}
+    />
+    <Tab.Screen
+      name={routes.LISTING_EDIT}
+      component={ListingEditScreen}
+      options={({ navigation }) => ({
+        tabBarButton: () => (
+          <NewListingButton
+            onPress={() => navigation.navigate(routes.LISTING_EDIT)}
+          />
+        ),
+        tabBarIcon: ({ color, size }) => (
+          <MaterialCommunityIcons
+            name="plus-circle"
+            color={color}
+            size={size}
+          />
+        ),
+      })}
+    />
+    <Tab.Screen
+      name={routes.ACCOUNTS}
+      component={AccountStackNavigator}
+      options={{
+        tabBarIcon: ({ color, size }) => (
+          <MaterialCommunityIcons name="account" color={color} size={size} />
+        ),
+      }}
+    />
+  </Tab.Navigator>
+);
+
+export default AppTabNavigator;
