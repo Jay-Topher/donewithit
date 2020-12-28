b8395026a404baf37d3db4d05b6e55beed9e4f04
Jones Ogolo <jay.topher11@gmail.com>
2020-12-28 23:57:31 +0100

    testing app

--- App.js
+++ App.js
@@ -2,11 +2,29 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import myTheme from "./app/navigation/navigationTheme";
 import AppTabNavigator from "./app/navigation/AppNavigator";
+import NetInfo, { useNetInfo } from "@react-native-community/netinfo";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function App() {
-  return (
-    <NavigationContainer theme={myTheme}>
-      <AppTabNavigator />
-    </NavigationContainer>
-  );
+  // return (
+  //   <NavigationContainer theme={myTheme}>
+  //     <AppTabNavigator />
+  //   </NavigationContainer>
+  // );
+
+  const demo = async () => {
+    try {
+      await AsyncStorage.setItem("person", JSON.stringify({ id: 1 }));
+      const value = await AsyncStorage.getItem("person");
+      const person = JSON.parse(value);
+      console.log(person);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  demo();
+  // const netInfo = useNetInfo();
+  // console.log(netInfo);
+  return null;
 }
