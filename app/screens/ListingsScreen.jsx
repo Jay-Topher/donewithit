bdcb2566f7e9978ce2f7037788cf29c042df523d
Jones Ogolo <jay.topher11@gmail.com>
2020-12-28 23:56:52 +0100

    removed sample listing

--- app/screens/ListingsScreen.jsx
+++ app/screens/ListingsScreen.jsx
@@ -1,28 +1,33 @@
-import React from "react";
-import { FlatList, Platform, StyleSheet, Text, View } from "react-native";
-import Card from "../components/Card";
-import Screen from "../components/Screen";
-import colors from "../config/colors";
-import Constants from "expo-constants";
-import routes from "../navigation/routes";
+import React, { useEffect, useState } from "react";
+import { FlatList, StyleSheet } from "react-native";
 
-const listings = [
-  {
-    id: 1,
-    title: "Red jacket for sale",
-    price: 100,
-    image: require("../assets/jacket.jpg"),
-  },
-  {
-    id: 2,
-    title: "Couch in great condition",
-    price: 1000,
-    image: require("../assets/couch.jpg"),
-  },
-];
+import ActivityIndicator from "../components/ActivityIndicator";
+import AppText from "../components/AppText";
+import AppButton from "../components/AppButton";
+import Card from "../components/Card";
+import colors from "../config/colors";
+import listingApi from "../api/listings";
+import routes from "../navigation/routes";
+import Screen from "../components/Screen";
+import useApi from "../hooks/useApi";
+
 const ListingsScreen = ({ navigation }) => {
+  const { data: listings, error, loading, request: loadListings } = useApi(
+    listingApi.getListings
+  );
+
+  useEffect(() => {
+    loadListings();
+  }, []);
   return (
     <Screen style={styles.container} outerStyle={styles.outer}>
+      {error && (
+        <>
+          <AppText>Couldn't retrieve the listings.</AppText>
+          <AppButton title="Retry" onPress={loadListings} />
+        </>
+      )}
+      <ActivityIndicator visible={loading} />
       <FlatList
         data={listings}
         keyExtractor={(listing) => listing.id.toString()}
@@ -30,7 +35,7 @@ const ListingsScreen = ({ navigation }) => {
           <Card
             title={item.title}
             subTitle={"$" + item.price}
-            image={item.image}
+            imageUrl={item.images[0].url}
             onPress={() => navigation.navigate(routes.LISTING_DETAILS, item)}
           />
         )}
@@ -46,8 +51,5 @@ const styles = StyleSheet.create({
   container: {
     // padding: 20,
     backgroundColor: colors.light,
-  },
-  outer: {
-    paddingTop: Platform.OS === "android" ? 0 : Constants.statusBarHeight,
   },
 });
