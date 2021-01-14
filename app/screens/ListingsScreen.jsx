0614ec8ae98cbd26c5cbde6967386d2a20fd07a1
Jones Ogolo <jay.topher11@gmail.com>
2021-01-14 15:16:08 +0100

    moved activity indicator to top

--- app/screens/ListingsScreen.jsx
+++ app/screens/ListingsScreen.jsx
@@ -20,29 +20,31 @@ const ListingsScreen = ({ navigation }) => {
     loadListings();
   }, []);
   return (
-    <Screen style={styles.container} outerStyle={styles.outer}>
-      {error && (
-        <>
-          <AppText>Couldn't retrieve the listings.</AppText>
-          <AppButton title="Retry" onPress={loadListings} />
-        </>
-      )}
+    <>
       <ActivityIndicator visible={loading} />
-      <FlatList
-        data={listings}
-        keyExtractor={(listing) => listing.id.toString()}
-        renderItem={({ item }) => (
-          <Card
-            title={item.title}
-            subTitle={"$" + item.price}
-            imageUrl={item.images[0].url}
-            onPress={() => navigation.navigate(routes.LISTING_DETAILS, item)}
-            thumbnailUrl={item.images[0].thumbnailUrl}
-          />
+      <Screen style={styles.container} outerStyle={styles.outer}>
+        {error && (
+          <>
+            <AppText>Couldn't retrieve the listings.</AppText>
+            <AppButton title="Retry" onPress={loadListings} />
+          </>
         )}
-        style={{ padding: 20 }}
-      />
-    </Screen>
+        <FlatList
+          data={listings}
+          keyExtractor={(listing) => listing.id.toString()}
+          renderItem={({ item }) => (
+            <Card
+              title={item.title}
+              subTitle={"$" + item.price}
+              imageUrl={item.images[0].url}
+              onPress={() => navigation.navigate(routes.LISTING_DETAILS, item)}
+              thumbnailUrl={item.images[0].thumbnailUrl}
+            />
+          )}
+          style={{ padding: 20 }}
+        />
+      </Screen>
+    </>
   );
 };
 
