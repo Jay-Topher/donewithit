c031898f04c8e0c08538d031241a8f80b946a07d
Jones Ogolo <jay.topher11@gmail.com>
2021-01-14 15:11:44 +0100

    created registration api call

--- app/api/auth.js
+++ app/api/auth.js
@@ -1,8 +1,12 @@
 import client from "./client";
 
 const endpoint = "/auth";
+const registrationEndpoint = "/users";
 const login = (email, password) => client.post(endpoint, { email, password });
+
+const register = (userInfo) => client.post(registrationEndpoint, userInfo);
 
 export default {
   login,
+  register,
 };
